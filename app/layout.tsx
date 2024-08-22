e08dc22d9385114114f168468313d08093d317e0
Daniel Tonon <dan503.npm@gmail.com>
2024-08-22 12:20:30 +1000

    Added Suspense wrapper to layout to fix precompiler error

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { AuthContextProvider } from '../components/AuthContext'
+import { Suspense } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -23,7 +24,9 @@ export default function RootLayout({
 		<html lang="en">
 			<body className={inter.className}>
 				<main>
-					<AuthContextProvider>{children}</AuthContextProvider>
+					<Suspense fallback={<p>Loading...</p>}>
+						<AuthContextProvider>{children}</AuthContextProvider>
+					</Suspense>
 				</main>
 			</body>
 		</html>
